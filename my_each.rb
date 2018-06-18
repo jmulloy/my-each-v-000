8a12b990b11663dc3cbb2b9a9d6e3af923cfcd97
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 17:30:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 require "pry"
 def my_each(my_statement) # put argument(s) here
   i = 0
-  while i < my_statement.length
+  while i < my_statement.split(" ")
     yield my_statement |i|
     i = i + 1
   end
