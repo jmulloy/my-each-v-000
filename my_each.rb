e83fc0caa55dd0a3db4e8535a296ab80fc825017
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 17:43:43 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ require "pry"
 def my_each(collection) # put argument(s) here
   i = 0
   while i < collection.length
-    yield collection[i]
+    yield(collection[i])
     i = i + 1
   end
   # code here
